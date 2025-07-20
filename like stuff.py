a1ac19753b9236fd28adc50f73ab8b646342121d
Elliott Lieberman <elliottlieberman123@gmail.com>
2025-07-20 16:04:44 -0400

    Added user input

--- like stuff.py
+++ like stuff.py
@@ -1,4 +1,4 @@
-import random, hangman_stages
+import random, hangman_stages, sys
 
 def select_word(words):
     return random.choice(words)
@@ -6,12 +6,30 @@ def select_word(words):
 def print_secret_word(secret_word):
     print(" _ " * len(secret_word))
 
-words = ("barzal", "Lee", "Sinner", "Varlomov", "Mahomes")
+def is_guess_in_secret_word(guess, secret_word):
+    if len(guess) > 1 or not guess.isalpha():
+        print("Only single letters are allowed. Unable to continue")
+        sys.exit()
+    else:
+        if guess in secret_word:
+            return True
+        else:
+            return False
+
+
+words = ("barzal", "lee", "sinner", "varlomov", "mahomes")
 remaining_attempts = 6
 guessed_letters = ""
 
 print("Welcome to SportsPlayerNameHangman!")
-
 secret_word = select_word(words)
+guess = input("Guess a letter: ")
+guess_in_secret_word = is_guess_in_secret_word(guess, secret_word)
+
+if guess_in_secret_word:
+    if guess in guessed_letters
+        print("You have already guessed this letter try again ()".format(guess))
+
+        
 print(hangman_stages.get_hangman_stage(remaining_attempts))
 print_secret_word(secret_word)
