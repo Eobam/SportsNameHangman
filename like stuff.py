e130bc90e982171ad85779728e5fac6bb0a3ca1c
Elliott Lieberman <elliottlieberman123@gmail.com>
2025-07-20 10:19:45 -0400

    ummm this might break it... But fixed "print(" _ " len(secret_word))" and probably broke everythign else

--- like stuff.py
+++ like stuff.py
@@ -3,15 +3,20 @@ import random
 def select_word(words):
     return random.choice(words)
 
-def print_secret_word(secret_word):
-    print(" _ " len(secret_word))
+def print_secret_word(secret_word, guessed_letters):
+    display = ""
+    for letter in secret_word:
+        if letter.lower() in guessed_letters:
+            display += f"{letter} "
+        else:
+            display += "_ "
+    print(display.strip())
 
 words = ("barzal", "Lee", "Sinner", "Varlomov", "Mahomes")
 remaining_attempts = 6
 guessed_letters = ""
 
-print(select_word(words))
+print("Welcome to SportsPlayerNameHangman!")
 
-print("Welecome to SportsPlayerNameHangman!")
 secret_word = select_word(words)
-print_secret_word(secret_word)
+print_secret_word(secret_word, guessed_letters)
