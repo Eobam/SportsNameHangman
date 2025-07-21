ffede659382df39f377bcac6aabeccfe8aa77de8
Elliott Lieberman <elliottlieberman123@gmail.com>
2025-07-21 13:51:36 -0400

    Fixed input bug

--- like stuff.py
+++ like stuff.py
@@ -6,21 +6,17 @@ def select_word(words):
 def print_secret_word(secret_word, guessed_letters):
     for letter in secret_word:
         if letter in guessed_letters:
-            print(" {} ".format(letter), ends="")
+            print(" {} ".format(letter), end="")
         else:
-            print(" _ ", ends="")
-    print("/n")
+            print(" _ ", end="")
+    print("\n")
     print(" _ " * len(secret_word))
 
 def is_guess_in_secret_word(guess, secret_word):
     if len(guess) > 1 or not guess.isalpha():
-        print("Only single letters are allowed. Unable to continue")
+        print("Only single letters are allowed. Unable to continue.")
         sys.exit()
-    else:
-        if guess in secret_word:
-            return True
-        else:
-            return False
+    return guess in secret_word
 
 def get_unique_letters(word):
     return "".join(set(word))
@@ -32,21 +28,22 @@ guessed_letters = ""
 print("Welcome to SportsPlayerNameHangman!")
 secret_word = select_word(words)
 
-while remaining_attempts > 0 and len(guessed_letters) < len(get_unique_letters(secret_word)):
-    guess = input("Guess a letter: ")
-    guess_in_secret_word = is_guess_in_secret_word(guess, secret_word)
+while remaining_attempts > 0 and len(set(guessed_letters) & set(secret_word)) < len(set(secret_word)):
+    guess = input("Guess a letter: ").lower()
 
-    if guess_in_secret_word:
-        if guess in guessed_letters:
-            print("You have already guessed this letter. Try again: {}".format(guess))
-        else:
-            print("Correct! The letter {} is part of the name.".format(guess))
-        guessed_letters += guess
+    if guess in guessed_letters:
+        print("You have already guessed this letter. Try again: {}".format(guess))
+        continue
+
+    if is_guess_in_secret_word(guess, secret_word):
+        print("Correct! The letter {} is part of the name.".format(guess))
     else:
         print("Incorrect! The letter {} is not in the word.".format(guess))
-    remaining_attempts -= 1
-        
+        remaining_attempts -= 1
+
+    guessed_letters += guess
+
     print(hangman_stages.get_hangman_stage(remaining_attempts))
     print("\n{} attempts remaining\n".format(remaining_attempts))
     print_secret_word(secret_word, guessed_letters)
-    print("\n\Number of letters guessed: {}\n".format(remaining_attempts))
+    print("\nLetters guessed so far: {}\n".format(guessed_letters))
