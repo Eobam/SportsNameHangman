7c375061d78eeffedd5c3d2d2f6ef52d25976122
Elliott Lieberman <elliottlieberman123@gmail.com>
2025-07-22 20:14:03 -0400

    blank

--- like_stuff.py
+++ like_stuff.py
@@ -3,6 +3,11 @@ import pygame
 
 pygame.init()
 
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Hangman Image Test")
+
+
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sports Name Hangman")
